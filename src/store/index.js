1fa2ade1ebef17222349e2b03961535d383ddd33
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-02-28 22:49:40 +0200

    added redux store enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,28 @@
 import { createStore } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
-import {combineReducers} from "@reduxjs/toolkit";
+import {combineReducers, compose} from "@reduxjs/toolkit";
 
-const store = createStore(combineReducers({heroes, filters}),
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+      if (typeof action === 'string') {
+        return oldDispatch({type: action});
+      }
+      return oldDispatch(action);
+    };
+    return store;
+}
+
+const store = createStore(
+  combineReducers({heroes, filters}),
+  compose(
+    enhancer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
 
 export default store;
+
+//
